b6916f3f22823eed8ed4be770bd87ef978ae50d6
rvp <ravaprayoga9@gmail.com>
2026-05-05 22:53:34 +0700

    Delete pages directory

--- pages/templates/sidebar_admin.php
+++
@@ -1,7 +0,0 @@
-<aside class="sidebar">
-    <div class="sidebar-header">
-        <h3>BILGI EVI</h3>
-        <p>Admin: <?= htmlspecialchars($_SESSION['username'] ?? '') ?></p>
-    </div>
-    <?php include 'navigasi_admin.php'; ?>
-</aside>
